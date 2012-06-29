a0cf1b8933774c370dbe5a8e8a8fb99593cda338
Dante Soares <dms3@rice.edu>
2012-06-29 18:08:24 -0500

    Namespaced the routes

--- lib/commontator/routes.rb
+++ lib/commontator/routes.rb
@@ -1,11 +1,13 @@
 module ActionDispatch::Routing
   class Mapper
     def commontable
-      resources :comments, :only => [:index, :new, :create]
-      post 'subscribe', :to => 'subscriptions#create',
-                        :as => 'subscribe'
-      post 'unsubscribe', :to => 'subscriptions#destroy',
-                          :as => 'unsubscribe'
+      namespace :commontator do
+        resources :comments, :only => [:index, :new, :create]
+        post 'subscribe', :to => 'subscriptions#create',
+                          :as => 'subscribe'
+        post 'unsubscribe', :to => 'subscriptions#destroy',
+                            :as => 'unsubscribe'
+      end
     end
 
     def commentable
