de29caddcc25d257486f2ac5b65641aaac5c3b89
Dante Soares <dms3@rice.edu>
2012-07-05 14:54:01 -0500

    Oops

--- app/helpers/commontator/commontable_helper.rb
+++ app/helpers/commontator/commontable_helper.rb
@@ -1,6 +1,6 @@
 module Commontator
   module CommontableHelper
-    def commontator_link(commontable, commontator)
+    def commontable_thread_link(commontable, commontator)
       render(:partial => 'commontator/commontables/thread',
              :locals => {:commontable => commontable,
                          :commontator => commontator}).html_safe
--- lib/commontator/acts_as_commontable.rb
+++ lib/commontator/acts_as_commontable.rb
@@ -9,7 +9,7 @@ module Commontator
     module ClassMethods
       def acts_as_commontable(options = {})
         class_eval do
-          has_one :thread, :as => :commentable, :dependent => :destroy
+          has_one :thread, :as => :commontable, :dependent => :destroy
           has_many :comments, :through => :thread
           has_many :subscriptions, :through => :thread
           has_many :subscribers, :through => :thread
