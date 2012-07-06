9924d20a59b56694c852db145a1661298bb8d42a
Dante Soares <dms3@rice.edu>
2012-07-05 19:46:17 -0500

    Removed not-applicable copyright notices

--- test/unit/comment_thread_subscription_test.rb
+++ test/unit/comment_thread_subscription_test.rb
@@ -1,6 +1,3 @@
-# Copyright 2011-2012 Rice University. Licensed under the Affero General Public 
-# License version 3 or later.  See the COPYRIGHT file for details.
-
 require 'test_helper'
 
 class CommentThreadSubscriptionTest < ActiveSupport::TestCase
