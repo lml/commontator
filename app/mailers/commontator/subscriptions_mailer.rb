93d91145c6b34bfc69c953eb1e8f28d458870be4
Dante Soares <dms3@rice.edu>
2012-07-27 19:00:08 -0500

    Minor improvement

--- app/mailers/commontator/subscriptions_mailer.rb
+++ app/mailers/commontator/subscriptions_mailer.rb
@@ -6,7 +6,9 @@ module Commontator
     def comment_created(comment, recipients)
       setup_variables(comment, recipients)
 
-      mail(:bcc => @bcc, :from => @from, :subject => @subject)
+      mail :bcc => @bcc,
+           :from => @from,
+           :subject => @subject
     end
 
 protected
