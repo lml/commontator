f1262372182617947d11eebca3679f05e4274bf3
Dante Soares <dms3@rice.edu>
2014-04-20 21:01:09 -0500

    Remigrated dummy app

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -18,32 +18,29 @@ ActiveRecord::Schema.define(version: 3) do
     t.integer  "creator_id"
     t.string   "editor_type"
     t.integer  "editor_id"
-    t.integer  "thread_id",                      null: false
-    t.text     "body",                           null: false
+    t.integer  "thread_id",                     null: false
+    t.text     "body",                          null: false
     t.datetime "deleted_at"
-    t.integer  "cached_votes_total", default: 0
-    t.integer  "cached_votes_up",    default: 0
-    t.integer  "cached_votes_down",  default: 0
+    t.integer  "cached_votes_up",   default: 0
+    t.integer  "cached_votes_down", default: 0
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   add_index "commontator_comments", ["cached_votes_down"], name: "index_commontator_comments_on_cached_votes_down"
-  add_index "commontator_comments", ["cached_votes_total"], name: "index_commontator_comments_on_cached_votes_total"
   add_index "commontator_comments", ["cached_votes_up"], name: "index_commontator_comments_on_cached_votes_up"
-  add_index "commontator_comments", ["creator_id", "creator_type", "thread_id"], name: "index_c_c_on_c_id_and_c_type_and_t_id"
+  add_index "commontator_comments", ["creator_id", "creator_type", "thread_id"], name: "index_commontator_comments_on_c_id_and_c_type_and_t_id"
   add_index "commontator_comments", ["thread_id"], name: "index_commontator_comments_on_thread_id"
 
   create_table "commontator_subscriptions", force: true do |t|
-    t.string   "subscriber_type",             null: false
-    t.integer  "subscriber_id",               null: false
-    t.integer  "thread_id",                   null: false
-    t.integer  "unread",          default: 0, null: false
+    t.string   "subscriber_type", null: false
+    t.integer  "subscriber_id",   null: false
+    t.integer  "thread_id",       null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  add_index "commontator_subscriptions", ["subscriber_id", "subscriber_type", "thread_id"], name: "index_c_s_on_s_id_and_s_type_and_t_id", unique: true
+  add_index "commontator_subscriptions", ["subscriber_id", "subscriber_type", "thread_id"], name: "index_commontator_subscriptions_on_s_id_and_s_type_and_t_id", unique: true
   add_index "commontator_subscriptions", ["thread_id"], name: "index_commontator_subscriptions_on_thread_id"
 
   create_table "commontator_threads", force: true do |t|
@@ -56,7 +53,7 @@ ActiveRecord::Schema.define(version: 3) do
     t.datetime "updated_at"
   end
 
-  add_index "commontator_threads", ["commontable_id", "commontable_type"], name: "index_c_t_on_c_id_and_c_type", unique: true
+  add_index "commontator_threads", ["commontable_id", "commontable_type"], name: "index_commontator_threads_on_c_id_and_c_type", unique: true
 
   create_table "dummy_models", force: true do |t|
     t.datetime "created_at"
@@ -84,4 +81,3 @@ ActiveRecord::Schema.define(version: 3) do
   add_index "votes", ["voter_id", "voter_type", "vote_scope"], name: "index_votes_on_voter_id_and_voter_type_and_vote_scope"
 
 end
-
