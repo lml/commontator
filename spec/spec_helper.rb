e7a9d0796cbb81908f80e75f73af0a8a046f79ac
BM5k <me@bm5k.com>
2015-04-15 12:05:54 -0700

    :lipstick: indent these comments
    this fixes code folding in the atom editor.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -48,8 +48,8 @@ RSpec.configure do |config|
     mocks.verify_partial_doubles = true
   end
 
-# The settings below are suggested to provide a good initial experience
-# with RSpec, but feel free to customize to your heart's content.
+  # The settings below are suggested to provide a good initial experience
+  # with RSpec, but feel free to customize to your heart's content.
   # These two settings work together to allow you to limit a spec run
   # to individual examples or groups you care about by tagging them with
   # `:focus` metadata. When nothing is tagged with `:focus`, all examples
