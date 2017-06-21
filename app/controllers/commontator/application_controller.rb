b95573c7839013d7e81eb7fc17663af762756a00
Trip Tate <trip@pegulabs.com>
2017-06-20 23:28:27 -0400

    typo fix

--- app/controllers/commontator/application_controller.rb
+++ app/controllers/commontator/application_controller.rb
@@ -1,6 +1,6 @@
 module Commontator
   class ApplicationController < ActionController::Base
-    before_actions :set_user, :ensure_user
+    before_action :set_user, :ensure_user
 
     rescue_from SecurityTransgression, :with => lambda { head(:forbidden) }
 
