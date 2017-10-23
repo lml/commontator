08d91e87988443c17ee4afa1389372e9ed1d5c50
Dante Soares <dante.m.soares@rice.edu>
2017-10-23 10:35:25 -0500

    Don't set To field instead of setting it to Undisclosed Recipients

--- app/mailers/commontator/subscriptions_mailer.rb
+++ app/mailers/commontator/subscriptions_mailer.rb
@@ -3,15 +3,8 @@ module Commontator
     def comment_created(comment, recipients)
       setup_variables(comment, recipients)
 
-      mail(to: @to,
-           bcc: @bcc,
-           from: @from,
-           subject: t('commontator.email.comment_created.subject',
-                      creator_name: @creator_name,
-                      commontable_name: @commontable_name,
-                      comment_url: @comment_url)).tap do |message|
-        message.mailgun_recipient_variables = mailgun_recipient_variables(recipients) \
-          if uses_mailgun?
+      mail(@mail_params).tap do |message|
+        message.mailgun_recipient_variables = @mailgun_recipient_variables if @using_mailgun
       end
     end
 
@@ -22,42 +15,35 @@ module Commontator
       @thread = @comment.thread
       @creator = @comment.creator
 
+      @mail_params = { from: @thread.config.email_from_proc.call(@thread) }
+
+      @recipient_emails = recipients.map do |recipient|
+        Commontator.commontator_email(recipient, self)
+      end
+
+      @using_mailgun = Rails.application.config.action_mailer.delivery_method == :mailgun
+
+      if @using_mailgun
+        @recipients_header = :to
+        @mailgun_recipient_variables = {}.tap do |mailgun_recipient_variables|
+          @recipient_emails.each { |email| mailgun_recipient_variables[email] = {} }
+        end
+      else
+        @recipients_header = :bcc
+      end
+
+      @mail_params[@recipients_header] = @recipient_emails
+
       @creator_name = Commontator.commontator_name(@creator)
-
       @commontable_name = Commontator.commontable_name(@thread)
-
       @comment_url = Commontator.comment_url(@comment, main_app)
 
-      params = Hash.new
-      params[:comment] = @comment
-      params[:thread] = @thread
-      params[:creator] = @creator
-      params[:creator_name] = @creator_name
-      params[:commontable_name] = @commontable_name
-      params[:comment_url] = @comment_url
-
-      if uses_mailgun?
-        @to = recipient_emails(recipients)
-      else
-        @to = t('commontator.email.undisclosed_recipients')
-        @bcc = recipient_emails(recipients)
-      end
-
-      @from = @thread.config.email_from_proc.call(@thread)
-    end
-
-    def recipient_emails(recipients)
-      recipients.collect{ |s| Commontator.commontator_email(s, self) }
-    end
-
-    def mailgun_recipient_variables(recipients)
-      recipient_emails(recipients).each_with_object({}) do |user_email, memo|
-        memo[user_email] = {}
-      end
-    end
-
-    def uses_mailgun?
-      Rails.application.config.action_mailer.delivery_method == :mailgun
+      @mail_params[:subject] = t(
+        'commontator.email.comment_created.subject',
+        creator_name: @creator_name,
+        commontable_name: @commontable_name,
+        comment_url: @comment_url
+      )
     end
   end
 end
