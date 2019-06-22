5ec86acff16487e9b5c865fc6886256b0b833339
Dante Soares <dante.m.soares@rice.edu>
2019-06-22 15:01:03 -0500

    Fix for random test errors with MySQL

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -34,7 +34,9 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  # Fix for https://github.com/brianmario/mysql2/issues/947
+  # Testing for Mysql2: https://stackoverflow.com/a/2536743
+  config.use_transactional_fixtures = ActiveRecord::Base.connection.adapter_name != 'Mysql2'
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
