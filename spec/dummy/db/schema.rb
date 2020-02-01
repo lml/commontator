74b9bc95848efe107a391182c3ede70d9b24303b
Dante Soares <dante.m.soares@rice.edu>
2020-02-01 14:40:20 -0600

    Fix MySQL and try ruby 2.7.0 and Postgres 10

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -22,8 +22,8 @@ ActiveRecord::Schema.define(version: 13) do
     t.datetime "deleted_at"
     t.integer "cached_votes_up", default: 0
     t.integer "cached_votes_down", default: 0
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
     t.integer "parent_id"
     t.index ["cached_votes_down"], name: "index_commontator_comments_on_cached_votes_down"
     t.index ["cached_votes_up"], name: "index_commontator_comments_on_cached_votes_up"
@@ -37,8 +37,8 @@ ActiveRecord::Schema.define(version: 13) do
     t.integer "thread_id", null: false
     t.string "subscriber_type", null: false
     t.integer "subscriber_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
     t.index ["subscriber_id", "subscriber_type", "thread_id"], name: "index_commontator_subscriptions_on_s_id_and_s_type_and_t_id", unique: true
     t.index ["thread_id"], name: "index_commontator_subscriptions_on_thread_id"
   end
@@ -49,8 +49,8 @@ ActiveRecord::Schema.define(version: 13) do
     t.string "closer_type"
     t.integer "closer_id"
     t.datetime "closed_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
     t.index ["closer_type", "closer_id"], name: "index_commontator_threads_on_closer_type_and_closer_id"
     t.index ["commontable_type", "commontable_id"], name: "index_commontator_threads_on_c_id_and_c_type", unique: true
   end
