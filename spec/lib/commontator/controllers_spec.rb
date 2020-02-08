9e9b61b5864c2101f60c904454b9cf685a3d9f8d
Dante Soares <dante.m.soares@rice.edu>
2020-02-07 23:27:17 -0600

    Test that both ActionController::Base and ActionController::API receive the Commontator controller modules

--- spec/lib/commontator/controllers_spec.rb
+++ spec/lib/commontator/controllers_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Commontator::Controllers, type: :lib do
-  it 'adds shared helper to ActionController and subclasses' do
-    expect(ActionController::Base.helpers).to respond_to(:commontator_thread)
-    expect(DummyModelsController.helpers).to respond_to(:commontator_thread)
-  end
-
-  it 'adds commontator_thread_show to ActionController instances' do
-    expect(ActionController::Base.new.respond_to?(:commontator_thread_show, true)).to eq true
-    expect(DummyModelsController.new.respond_to?(:commontator_thread_show, true)).to eq true
+  {
+    ActionController::API => DummyApiController,
+    ActionController::Base => DummyModelsController
+  }.each do |klass, subclass|
+    it "adds commontator_thread_show to #{klass.name} instances" do
+      expect(klass.new.respond_to?(:commontator_thread_show, true)).to eq true
+      expect(subclass.new.respond_to?(:commontator_thread_show, true)).to eq true
+    end
   end
 end
